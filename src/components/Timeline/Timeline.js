aa5672a5d048a28906d47332bc0bc123c6eeac0b
Bruno Raniere <brunoraniere2@gmail.com>
2024-11-10 18:03:29 -0300

    ajustando texto

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -6,7 +6,7 @@ const events = [
   { title: "Primeira venda", description: "Aos 4 anos de idade fiz minha primeira venda, entregando ao mercado (minha família) adesivos de caderno." },
   { title: "Desenvolvendo nas vendas", description: "Aos 11 comecei a vender produtos importados para família e amigos, além de alguns conhecidos." },
   { title: "Recorde de vendas", description: "Aos 12 anos continuei comprando produtos importados e fui para a praia vender, em dois dias consegui fazer um total de 0 VENDAS, um belo baque." },
-  { title: "Primeiro golpe", description: 'Aos 13 fui começar a vender celulares semi-novos, no primeiro enviei 1300 reais para um iPhone 7 que nunca chegou. "Melhor 1300 agora do que 1M lá na frente". Estou em busca dos 1M rsrsrs.'},
+  { title: "Primeiro golpe", description: 'Aos 13 fui começar a vender celulares semi-novos, no primeiro enviei 1300 reais por um iPhone 7 que nunca chegou. "Melhor perder 1300 agora do que 1M lá na frente". Agora estou em busca dos 1M rsrsrs.'},
   { title: "No mundo da tecnologia", description: 'Aos 14 entrei no IFRN onde comecei a investir em ações, cripto e principalmente a programar.'},
   { title: "Minha empresa", description: 'Aos 16 montei minha primeira empresa, Lockers! Uma empresa de armários inteligentes na Praia de Ponta Negra. Sucesso total!'},
   { title: "Mais estudo", description: 'Aos 18 servi por um tempo o NPOR do Exército Brasileiro e entrei na UFRN para cursar Tecnologia da Informação.'},
